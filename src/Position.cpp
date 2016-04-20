8aa7f331ed55c2cbf65c02cfcf30a2525958aa22
Nicolai Czempin <nicolai.czempin@gmail.com>
2016-04-20 13:54:19 +0200

    Start setting squares

--- src/Position.cpp
+++ src/Position.cpp
@@ -64,7 +64,6 @@ shared_ptr<Position> Position::create_position(const string& fen)
           //Move_generator::set_square(f, r, start_position->pawns);
           break;
         }
-
       }
     }
   }
