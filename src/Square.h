f42d91126a53057097c698ce28b93f8c2089214e
Nicolai Czempin <nicolai.czempin@gmail.com>
2016-04-30 17:21:16 +0200

    Add dummy castling moves

--- src/Square.h
+++ src/Square.h
@@ -11,12 +11,15 @@
 
 using namespace std;
 
-class Square {
+class Square
+{
 public:
   Square();
   virtual ~Square();
   static void print_square(uint8_t square);
   static string mailbox_index_to_square(int square);
+  static const int E1 = 4;
+  static const int E8 = 64 - 4;
 
 };
 
