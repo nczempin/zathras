9704dcc77fd33a9866198076b9d3c74837fce338
Nicolai Czempin <nicolai.czempin@gmail.com>
2016-05-22 00:13:17 +0200

    Set up position and depth

--- src/Perft_command.cpp
+++ src/Perft_command.cpp
@@ -150,10 +150,10 @@ void Perft_command::execute()
 //
 //  exit(0);
   vector<string> path = receiver->getArguments();
-  int depth =7; //TODO get this from arguments, but use a reasonable default
+  int depth = 6; //TODO get this from arguments, but use a reasonable default
   Position position;
   position = Position::create_position(
-      "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
+      "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
   //position = Position::create_start_position();
   cout << "Perft " << depth << " for this position: " << endl;
   cout << position << endl;
